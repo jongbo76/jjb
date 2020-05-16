2879b8d95ec611c161053fa9f6abafac4e58d38a
jongbo76 <jongbo76@naver.com>
2020-05-16 09:43:11 +0900

    raining

--- Weather.js
+++ Weather.js
@@ -8,7 +8,11 @@ export default class Weather extends Component {
       <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
        <View style={styles.upper}>
            <Text> Icon here!</Text>
-           <Text> Temp here!</Text>
+           <Text style={styles.temp}>35°</Text>
+       </View>
+       <View style={styles.lower}>
+         <Text style={styles.title}>Raining like a MF</Text>
+         <Text style={styles.subtitle}>For more info look outsie</Text>
        </View>
       </LinearGradient>
     );
@@ -23,6 +27,31 @@ export default class Weather extends Component {
         flex:1,
         alignItems:"center",
         justifyContent: "center"
+    },
+    temp:{
+      fontSize:24,
+      backgroundColor:"transparent",
+      color: "white",
+      marginTop:10
+    },
+    lower:{
+      flex:1,
+      alignItems: "flex-start",
+      justifyContent: "flex-end",
+      paddingLeft: 25
+    },
+    title:{
+      fontSize: 38,
+      backgroundColor:"transparent",
+      color:"white",
+      marginBottom:10,
+      fontWeight: "300"
+    },
+    subtitle:{
+      fontSize:24,
+      backgroundColor:"transparent",
+      color: "white",
+      marginBottom: 24
     }
   });
   
